59b87b3b685959b26bd0424a62e06c5bba3ba294
hgq1103623035 <2553901781@qq.com>
2016-02-19 10:34:17 +0800

    Initial Commit

---
+++ KVOTableBarManger/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KVOTableBarManger
+//
+//  Created by mac on 16/2/19.
+//  Copyright © 2016年 Myteam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KVOTableBarManger/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KVOTableBarManger
+//
+//  Created by mac on 16/2/19.
+//  Copyright © 2016年 Myteam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
